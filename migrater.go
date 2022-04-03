86f914ca0d073664ac05c1d3b86075ec7554867b
urydmi <urydmi@gmail.com>
2022-04-03 22:53:02 +0700

    Helpers moved to the one file

--- migrater.go
+++ migrater.go
@@ -2,13 +2,8 @@ package migorm
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-	"path"
-	"runtime"
-	"sort"
 	"strings"
-	"text/template"
 	"time"
 
 	"gorm.io/gorm"
@@ -225,48 +220,4 @@ func (m *migrater) createMigrationsTable() (bool, error) {
 
 	m.Log.Infof("Init table: %v", m.TableName)
 	return true, nil
-}
-
-func getMigrationNames() []string {
-	names := make([]string, 0, len(pool.migrations))
-	for k := range pool.migrations {
-		names = append(names, k)
-	}
-
-	sort.Strings(names)
-
-	return names
-}
-
-// сheck the existence of a file in the directory with migrations
-func checkFileExists(dir string, name string) error {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return err
-	}
-
-	for _, f := range files {
-		split := strings.Split(f.Name(), "_")
-
-		if name == strings.Join(split[1:], "_") {
-			return fmt.Errorf("file %v already exists in dir: %v", name, dir)
-		}
-	}
-
-	return nil
-}
-
-//
-func getTemplate() (*template.Template, error) {
-	_, filename, _, ok := runtime.Caller(1)
-	if !ok {
-		return nil, fmt.Errorf("template caller")
-	}
-
-	tmpl, err := template.ParseFiles(path.Dir(filename) + "/" + "template")
-	if err != nil {
-		return nil, fmt.Errorf("parse template : %v", err)
-	}
-
-	return tmpl, nil
 }
